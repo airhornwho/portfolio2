65649f43e6431ea1b032f676d43fa8e6515ca66f
Aaron <1kookyw1@gmail.com>
2024-02-12 20:54:59 -0800

    added bento grid

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,49 @@
 import React from 'react'
 import { Link, Button, Card, CardBody, Image, Chip } from "@nextui-org/react";
+import ProfileCard from '../components/profileCard';
 
 
 const Page = () => {
   return (
 	<>
-	<div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_20%,rgba(130,119,238,0.3),rgba(255,255,255,0))]"></div>
+	<div className="absolute top-0 z-[-2] h-full w-screen bg-neutral-800 bg-[radial-gradient(ellipse_80%_80%_at_50%_30%,rgba(130,119,108,0.7),rgba(255,255,255,0))]"></div>
 	<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-		<Card isBlurred isHoverable className="border-none bg-slate-950" shadow="sm">
-			<CardBody>
-				<div className="flex flex-col w-full">
-					<div className="flex justify-center">
-						<Image src="/image.png" width={200} height={200} alt="image"
-						className="justify-center rounded-lg border-large"/>
-					</div>
-					<div className="flex justify-center pt-2">
-						<h2 className="text-3xl font-bold">airhorn</h2>
-					</div>
-					<div className="flex justify-center m-4 gap-4">
-						<Chip variant="bordered" color="secondary" size="sm">
-							Github
-						</Chip>
-						<Chip variant="bordered" color="secondary" size="sm">
-							Linkedin
-						</Chip>
-						<Chip variant="bordered" color="secondary" size="sm">
-							Gmail
-						</Chip>
-					</div>
-				</div>
-			</CardBody>
-		</Card>
 	</section>
+	<div className="flex justify-center">
+		<section className="grid grid-cols-12 gap-3 max-w-[1200px] p-6">
+			<div className="col-span-5 row-span-2 bg-red-50">
+				<Card className="flex flex-col p-6 h-[200px] ">
+					<p>My name is Aaron</p>
+				</Card>
+			</div>
+			
+			<div className="col-span-3 row-span-2 bg-red-50">
+				<Card className="flex flex-col p-6 h-[200px] ">
+					<p>Name</p>
+				</Card>
+			</div>
+			<div className="col-span-4 row-span-4 bg-red-50">
+				<Card className="flex flex-col p-6 h-[200px] ">
+					<p>Name</p>
+				</Card>
+			</div>
+			<div className="col-span-6 row-span-2 bg-red-50">
+				<Card className="flex flex-col p-6 h-[200px] ">
+					<p>Name</p>
+				</Card>
+			</div>
+			<div className="col-span-2 row-span-2 bg-red-50">
+				<Card className="flex flex-col p-6 h-[200px] ">
+					<p>Name</p>
+				</Card>
+			</div>
+			
+			
+			
+
+		</section>
+	</div>
+	
 	</>
   )
 }
---
+++ components/profileCard.tsx
@@ -0,0 +1,33 @@
+import { Card, CardBody, Image, Chip } from '@nextui-org/react'
+import React from 'react'
+
+const ProfileCard = () => {
+  return (
+    <Card isBlurred isHoverable className="border-none bg-slate-950" shadow="sm">
+			<CardBody>
+				<div className="flex flex-col w-full">
+					<div className="flex justify-center">
+						<Image src="/image.png" width={200} height={200} alt="image"
+						className="justify-center rounded-lg border-large"/>
+					</div>
+					<div className="flex justify-center pt-2">
+						<h2 className="text-3xl font-bold">airhorn</h2>
+					</div>
+					<div className="flex justify-center m-4 gap-2">
+						<Chip variant="bordered" color="secondary" size="sm">
+							Github
+						</Chip>
+						<Chip variant="bordered" color="secondary" size="sm">
+							Linkedin
+						</Chip>
+						<Chip variant="bordered" color="secondary" size="sm">
+							Gmail
+						</Chip>
+					</div>
+				</div>
+			</CardBody>
+		</Card>
+  )
+}
+
+export default ProfileCard
